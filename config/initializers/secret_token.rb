32c89dcc773f31c33a2bc8309b8176d97962c0c4
chiastolite <chiastolite.1980@gmail.com>
2013-08-01 22:16:36 +0900

    whitesnake test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhitesnakeTest::Application.config.secret_key_base = '526fe12efd37629d2a1abc78d6ee3703197cb38a391e5277bd4ae79b7f60ea4753d955ec8be277606ab1990d454963708ff638d7e0af2d2ebe8a532a65d64205'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhitesnakeTest::Application.config.session_store :cookie_store, key: '_whitesnake_test_session'
